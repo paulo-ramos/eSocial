966c567db597be944706b064894114e1b319a03f
paulo-ramos <paulo_ramos@live.com>
2024-10-28 22:14:07 -0300

    Add configurações IoC, saneamento da class program

---
+++ modulos/S1000/src/Ramos.eSocial.S1000.Api/IoC/ServicesExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json.Serialization;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Ramos.eSocial.S1000.Application.Commands;
+using Ramos.eSocial.S1000.Application.Handlers;
+using Ramos.eSocial.S1000.Application.Mediator;
+using Ramos.eSocial.S1000.Application.Validators;
+using Ramos.eSocial.S1000.Domain.Interfaces;
+using Ramos.eSocial.S1000.Infrastructure.Database;
+using Ramos.eSocial.S1000.Infrastructure.Repositories;
+using Ramos.eSocial.S1000.Shared.Handlers;
+
+namespace Ramos.eSocial.S1000.Api.IoC;
+
+public static class ServicesExtensions
+{
+    public static void AddServices(this IServiceCollection services)
+    {
+        // Adiciona as opções JSON personalizadas
+        services.Configure<JsonOptions>(options =>
+        {
+            options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+            options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+            options.JsonSerializerOptions.UnknownTypeHandling = JsonUnknownTypeHandling.JsonNode; 
+        });
+        
+        // Add services to the container.
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        
+        // Add services
+        services.AddSingleton<MongoDbContext>();
+        services.AddScoped<IDadosEmpregadorRepository, DadosEmpregadorRepository>();
+        services.AddScoped<IMediator, Mediator>();
+        services.AddScoped<ICommandHandler<CreateDadosEmpregadorCommand>, CreateDadosEmpregadorCommandHandler>();
+        services.AddScoped<ICommandHandler<UpdateDadosEmpregadorCommand>, UpdateDadosEmpregadorCommandHandler>();
+        services.AddScoped<ICommandHandler<FetchDadosEmpregadorCommand>, FetchDadosEmpregadorCommandHandler>();
+        services.AddTransient<IValidator<CreateDadosEmpregadorCommand>, CreateDadosEmpregadorCommandValidator>();
+        services.AddTransient<IValidator<UpdateDadosEmpregadorCommand>, UpdateDadosEmpregadorCommandValidator>();
+        services.AddTransient<IValidator<FetchDadosEmpregadorCommand>, FetchDadosEmpregadorCommandValidator>();
+        
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+}
--- modulos/S1000/src/Ramos.eSocial.S1000.Api/Program.cs
+++ modulos/S1000/src/Ramos.eSocial.S1000.Api/Program.cs
@@ -1,55 +1,19 @@
-using System.Text.Json.Serialization;
-using FluentValidation;
-using Microsoft.AspNetCore.Mvc;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
-using Ramos.eSocial.S1000.Application.Commands;
-using Ramos.eSocial.S1000.Application.Handlers;
-using Ramos.eSocial.S1000.Application.Mediator;
-using Ramos.eSocial.S1000.Application.Validators;
-using Ramos.eSocial.S1000.Domain.Interfaces;
-using Ramos.eSocial.S1000.Infrastructure.Database;
+using Ramos.eSocial.S1000.Api.IoC;
 using Ramos.eSocial.S1000.Infrastructure.Middleware;
-using Ramos.eSocial.S1000.Infrastructure.Repositories;
-using Ramos.eSocial.S1000.Shared.Handlers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adiciona as opções JSON personalizadas
-builder.Services.Configure<JsonOptions>(options =>
-{
-    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
-    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-//    options.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
-    options.JsonSerializerOptions.UnknownTypeHandling = JsonUnknownTypeHandling.JsonNode; // Adiciona isso para ignorar campos extras
-});
-
-
-// Registrar o serializador de GUID globalmente
-BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
-
-// Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddSingleton<MongoDbContext>();
-builder.Services.AddScoped<IDadosEmpregadorRepository, DadosEmpregadorRepository>();
-//builder.Services.AddScoped<IPessoaService, PessoaService>(); // Supondo que você tenha um serviço também
-builder.Services.AddScoped<IMediator, Mediator>();
-builder.Services.AddScoped<ICommandHandler<CreateDadosEmpregadorCommand>, CreateDadosEmpregadorCommandHandler>();
-builder.Services.AddScoped<ICommandHandler<UpdateDadosEmpregadorCommand>, UpdateDadosEmpregadorCommandHandler>();
-builder.Services.AddScoped<ICommandHandler<FetchDadosEmpregadorCommand>, FetchDadosEmpregadorCommandHandler>();
-builder.Services.AddTransient<IValidator<CreateDadosEmpregadorCommand>, CreateDadosEmpregadorCommandValidator>();
-builder.Services.AddTransient<IValidator<UpdateDadosEmpregadorCommand>, UpdateDadosEmpregadorCommandValidator>();
-builder.Services.AddTransient<IValidator<FetchDadosEmpregadorCommand>, FetchDadosEmpregadorCommandValidator>();
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddServices();
 
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+// Registre os endpoints diretamente no nível superior
+app.MapControllers();
+app.MapEndpoints(); // Chame o método para mapear os endpoints personalizado
 
 app.UseMiddleware<RequestIdMiddleware>();
 
@@ -59,62 +23,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-
-app.MapPost("/empregador", async (CreateDadosEmpregadorCommand command, IMediator mediator, HttpContext context) =>
-    {
-        var processKey = context.Items["ProcessKey"];
-        var processDate = context.Items["ProcessDate"];
-        
-        var result = await mediator.Send(command);
-        if (!result.Success)
-        {
-            return Results.BadRequest(new { Success = result.Success, Message = result.Message, ProcessKey = processKey, ProcessDate = processDate});
-        }
-
-        return Results.Ok(new { Success = result.Success, Message = result.Message, ProcessKey = processKey, ProcessDate = processDate });
-    })
-.WithName("PostDadosEmpregador")
-.WithOpenApi();
-
-
-
-app.MapPut("/empregador", async (UpdateDadosEmpregadorCommand command, IMediator mediator, HttpContext context) =>
-    {
-        var processKey = context.Items["ProcessKey"];
-        var processDate = context.Items["ProcessDate"];
-        
-        var result = await mediator.Send(command);
-        if (!result.Success)
-        {
-            return Results.BadRequest(new { Success = result.Success, Message = result.Message, ProcessKey = processKey, ProcessDate = processDate});
-        }
-
-        return Results.Ok(new { Success = result.Success, Message = result.Message, ProcessKey = processKey, ProcessDate = processDate });
-    })
-    .WithName("PutDadosEmpregador")
-    .WithOpenApi();
-
-
-app.MapGet("/empregador/{nrInsc}", async (IMediator mediator, string nrInsc, HttpContext context) =>
-    {
-        var processKey = context.Items["ProcessKey"];
-        var processDate = context.Items["ProcessDate"];
-        
-        var command = new FetchDadosEmpregadorCommand(nrInsc);
-        var result = await mediator.Send(command);
-        if (!result.Success)
-        {
-            return Results.BadRequest(new { Success = result.Success, Message = result.Message, ProcessKey = processKey, ProcessDate = processDate});
-        }
-
-        return Results.Ok(new { Data = result.Data, Success = result.Success, ProcessKey = processKey, ProcessDate = processDate });
-    })
-    .WithName("GetDadosEmpregador")
-    .WithOpenApi();
-
 
 app.Run();
 
